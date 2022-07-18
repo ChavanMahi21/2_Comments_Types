2337c282e0918701a6f8ebcd86e8e815f8ba1c57
ChavanMahi21 <109504569+ChavanMahi21@users.noreply.github.com>
2022-07-18 12:30:16 +0530

    Add files via upload

---
+++ comments.py
@@ -0,0 +1,9 @@
+'''
+Name: HD
+Date  & Day: 18th July 2022 (Monday)  
+File Desc: Types of comments in Python
+           1) Single Line Comment
+           2) Multi Line Comment
+'''
+# Its a single line comment
+print('Hey Today we have seen Comments in Python')
